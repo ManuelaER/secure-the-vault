b479612a3b8dcdf9b2ff8383f116e7b25fc0dcb6
Manuela <manuecherami@gmail.com>
2025-03-03 22:06:48 -0500

    final

---
+++ script.js
@@ -0,0 +1,7 @@
+// This are the values for the combination
+let num1 = (2 * 5)
+let num2 = (120 / 3)
+let num3 = (35 + 4)
+
+// pseudocode: create a dialog box that shows the vault combination
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`);
